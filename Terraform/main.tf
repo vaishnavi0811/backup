6b788e196cab04fee85af9c69c22672baf3b2a2f
Vaishu <vaisumathy@gmail.com>
2021-11-22 11:16:10 -0500

    Backup files

---
+++ Terraform/main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+	 features {}
+}
+
+resource "azurerm_virtual_network" "example" {
+    name = "azurevnet"
+    location = "south india"
+    resource_group_name = "Learning"
+    address_space = ["10.0.0.0/16"]
+    tags = {
+        environmet = "Test"
+    }
+}
+
+resource "azurerm_subnet" "example" {
+    name = "subnet0"
+    resource_group_name = "Learning"
+    virtual_network_name = "azurerm_virtual_network.example.name"
+    address_prefixes = ["10.0.1.0/24"]
+}
+
